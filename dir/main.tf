7575c413c520800a94c002fcac4d7cfd48ea9dd5
Lorna Song <lorna@hashicorp.com>
2020-07-15 11:07:54 -0400

    add consul kv backend

--- dir/main.tf
+++ dir/main.tf
@@ -1,5 +1,12 @@
 terraform {
     required_version = ">=0.12"
+
+    # Configuration block to use Consul as Terraform Backend
+    # backend "consul" {
+    #     address = "127.0.0.1:8500"
+    #     scheme  = "http"
+    #     path    = "network/terraform"
+    # }
 }
 
 module "local" {
